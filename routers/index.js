e620ed27922aefbbcc99da09c3ec431d36b6684d
Syarah Namira <syarahnamira@gmail.com>
2024-02-22 11:46:36 +0700

    bootstrap

--- routers/index.js
+++ routers/index.js
@@ -8,6 +8,7 @@ router.get('/', Controller.getRegister)
 router.post('/',Controller.postRegister)
 router.get('/login',Controller.getLogin)
 router.post('/login',Controller.postLogin)
+router.get('/home', Contoller.getHome)
 
 
 module.exports = router
