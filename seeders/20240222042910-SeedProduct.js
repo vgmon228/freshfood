1a3d9243e323c73159565d27a305888b79f5cadb
Syarah Namira <syarahnamira@gmail.com>
2024-02-22 11:31:59 +0700

    seeding product

---
+++ seeders/20240222042910-SeedProduct.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   let data = require("../data/products.json").map((perProduct)=>{
+     perProduct.createdAt = new Date()
+     perProduct.updatedAt = new Date()
+     return perProduct
+   })
+   await queryInterface.bulkInsert("Products", data, {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Products", null, {})
+  }
+};
